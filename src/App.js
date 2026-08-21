c1b6f044aa891a9a684bf4164d2f67e7519c200a
Kenfod <kelvinnyagudi@yahoo.com>
2026-08-21 20:55:29 +0300

    Add lifecycle methods to automate weather fetching and persist state: - Implement componentDidMount to read initial location from localStorage - Implement componentDidUpdate to auto-fetch weather data on location state change - Save updated search queries to localStorage automatically - Remove manual 'Get weather' submission button from layout - Clear weather forecast state when input is shorter than 2 characters - Add componentWillUnmount log to Weather component for cleanup tracking

--- src/App.js
+++ src/App.js
@@ -44,15 +44,16 @@ function formatDay(dateStr) {
 
 class App extends React.Component {
   state = {
-    location: "lisbon",
+    location: "",
     isLoading: false,
     displayLocationName: "",
     displayLocationCountryCode: "",
     weather: {},
   };
 
-  // async fetchWeather() {
   fetchWeather = async () => {
+    if (this.state.location.length < 2) return this.setState({ weather: {} });
+
     try {
       this.setState({ isLoading: true });
       // 1) Getting location (geocoding)
@@ -87,6 +88,22 @@ class App extends React.Component {
 
   setLocation = (e) => this.setState({ location: e.target.value });
 
+  // useEffect []
+  componentDidMount() {
+    // this.fetchWeather();
+
+    this.setState({ location: localStorage.getItem("location") || "" });
+  }
+
+  // useEffect[location]
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.location !== prevState.location) {
+      this.fetchWeather();
+
+      localStorage.setItem("location", this.state.location);
+    }
+  }
+
   render() {
     const { displayLocationName, displayLocationCountryCode } = this.state;
 
@@ -97,8 +114,6 @@ class App extends React.Component {
           location={this.state.location}
           onChangeLocation={this.setLocation}
         />
-
-        <button onClick={this.fetchWeather}>Get weather</button>
 
         {this.state.isLoading && <p className="loader">Loading...</p>}
 
@@ -132,6 +147,10 @@ class Input extends React.Component {
 }
 
 class Weather extends React.Component {
+  componentWillUnmount() {
+    console.log("Weather will unmount");
+  }
+
   render() {
     const {
       temperature_2m_max: max,
